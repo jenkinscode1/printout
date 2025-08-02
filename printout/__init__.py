e92c47aac44da78e7c9d688a38a0d9bfc0f47779
Jelani Jenkins <jelani.jenkins@charter.com>
2025-08-02 15:13:48 -0400

    version update

--- printout/__init__.py
+++ printout/__init__.py
@@ -1,23 +1,5 @@
-# jprint/__init__.py
-import os
-import sys
+# printout/__init__.py
 
-def printout(*args, logfile=None, **kwargs):
-    """
-    Works like print, but also logs to a file.
-    If logfile is None, defaults to <script_name>_log.log.
-    """
-    # Determine default log file
-    if logfile is None:
-        script_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
-        logfile = f"{script_name}_log.log"
+from .printout import printout
 
-    # Join args into a message
-    message = " ".join(str(arg) for arg in args)
-
-    # Print to console
-    print(message, **kwargs)
-
-    # Append to log file
-    with open(logfile, "a", encoding="utf-8") as f:
-        f.write(message + "\n")
+__all__ = ["printout"]
