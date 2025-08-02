f4ea7e30a62827cd826dcdcb95157ba7f358d559
Jelani Jenkins <jelani.jenkins@charter.com>
2025-08-02 17:05:00 -0400

    version updates

--- printout/__init__.py
+++ printout/__init__.py
@@ -1,8 +1,5 @@
 from .printout import printout
 
-# Make the package callable
-def __call__(*args, **kwargs):
-    return printout(*args, **kwargs)
-
-# Expose the function so "from printout import printLog" also works
-__all__ = ["printout"]
+# Make the module itself callable like a function
+import sys
+sys.modules[__name__] = printout
